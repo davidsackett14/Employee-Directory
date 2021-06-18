034fc000660e49644127bec6825eb037b6f65119
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-17 21:17:35 -0600

    add filtering option

--- src/pages/directory.jsx
+++ src/pages/directory.jsx
@@ -8,6 +8,7 @@ import fetchUsers from "../utils/userapi"
 function Table( ){
     const [people, setPeople] = useState([])
     const[input, setInput]= useState('')
+    const [order, setOrder]= useState(false)
 
     useEffect(() => {
         fetchUsers()
@@ -23,7 +24,7 @@ return(
     <thead>
       <tr>
         <th scope="col">Title</th>
-        <th scope="col">First</th>
+        <th scope="col">First <span className="text-muted" onClick={()=>setOrder(!order)}>{order?'z-a':'a-z'}</span></th>
         <th scope="col">Last</th>
         <th scope="col">Email</th>
         <th scope="col">Profile Picture</th>
@@ -31,7 +32,29 @@ return(
       </tr>
     </thead>
     <tbody>
-    {people.filter(person=> person.name.title.toLowerCase().startsWith(input.toLowerCase())).map(person=> <tr>
+    {people.filter(person=> person.name.title.toLowerCase().startsWith(input.toLowerCase())).sort((a, b)=>{
+    var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+    if(!order){
+        if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }   
+    }
+  else{
+    if (nameA > nameB) {
+        return -1;
+      }
+      if (nameA < nameB) {
+        return 1;
+      }
+  }
+    
+      // names must be equal
+      return 0;
+    }).map(person=> <tr>
     <th scope="row">{person.name.title}</th>
     <td>{person.name.first}</td>
     <td>{person.name.last}</td>
--- src/utils/userapi.js
+++ src/utils/userapi.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () => {
-  const res = await fetch("https://randomuser.me/api/?results=20");
+  const res = await fetch("https://randomuser.me/api/?results=20&nat=us");
   return await res.json();
 };
 export default fetchUsers;
